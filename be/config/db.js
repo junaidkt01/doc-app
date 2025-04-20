4d139b5dd9f9214c440b6e177a93f7657c43bc65
junaidkt01 <junaidktkmn@gmail.com>
2025-04-20 19:26:07 +0530

    changed credentials in db

--- be/config/db.js
+++ be/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "Junu@0138",
+  password: "password",
   database: "doc-app",
 });
 
