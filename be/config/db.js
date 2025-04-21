19a92b8ad6cfed261053dedab9445d8d52111841
junaidkt01 <junaidktkmn@gmail.com>
2025-04-21 11:30:16 +0530

    hosted mysql db

--- be/config/db.js
+++ be/config/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
